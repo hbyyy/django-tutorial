acdda65cffc0c80d11b7de9569169c000fa69ece
hbyyy <qjaduddl94@gmail.com>
2019-12-17 17:19:56 +0900

    detail, results, vote url 연결

--- polls/views.py
+++ polls/views.py
@@ -6,3 +6,16 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('hello django world!')
+
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}.")
+
+
+def results(request, question_id):
+    response = "you're looking at the results of question {}"
+    return HttpResponse(response.format(question_id))
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}")
