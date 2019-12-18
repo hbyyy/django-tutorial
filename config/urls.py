407cc43f4a32f34b5a38b96a93fc1fcf3352051e
hbyyy <qjaduddl94@gmail.com>
2019-12-18 21:49:51 +0900

    정리

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-# print("config입니다")
+# print("config입니")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
